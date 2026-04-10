784faa17afcd85b768fe0c126ff96ad2a398bade
= <=>
2026-04-10 21:10:15 +0500

    mening qismim tayyor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,83 @@
 import Logo from '../assets/Uztelecom.svg'
+
 export default function Footer() {
   return (
-    <footer className='bg-[#E9ECEF] '>
-      <div className="container px-30! lg:10!">
-        <div className='@media (width >= 40rem) { py-10px } flex items-center  gap-30 mt-20'>
+    <footer className='bg-[#E9ECEF] dark:bg-[#1a1a1a] transition-colors'>
+      <div className="container px-30 lg:px-10">
+
+        <div className='flex flex-col lg:flex-row items-start lg:items-center gap-10 lg:gap-30 mt-20'>
+
+          {/* Left */}
           <div>
             <img src={Logo} alt="logo" />
-            <p className='text-[16px] font-medium text-[#65696F]'> +998 55 500 09 90</p>
-            <a href="https://www.youtube.com/watch?v=oYVgmoJl8zQ&list=RDoYVgmoJl8zQ&start_radio=1" className='text-[16px] font-medium text-[#65696F]'>1c@uztelecom.uz</a>
-          </div>
-          <div className='flex gap-30'>
-            <div className='flex flex-col gap-3 border'>
-              <a href="" className='text-[16px] font-bold text-[#26282A]'>Xizmat xaqida</a>
-        <a href="" className='text-[16px] font-medium text-[#65696F] hover:text-black' >1C UZTELECOM nima va qanday ishlaydi?</a>
-        <a href=""className='text-[16px] font-medium text-[#65696F] hover:text-black' >Foydali funksiyalar</a>
-        <a href=""className='text-[16px] font-medium text-[#65696F] hover:text-black' >Foydalanish afzalliklari</a>
-              <a href=""className='text-[16px] font-medium text-[#65696F] hover:text-black'>Ishni qanday boshlash kerak</a>
-            </div>
-            
-            <div className='flex flex-col gap-3 '>
-              <a href='' className='text-[16px] font-bold text-[#26282A]'>Resurslar</a>
-       <a href=""className='text-[16px] font-bold text-[#65696F] hover:text-black'>Tariflar</a>
-       <a href=""className='text-[16px] font-bold text-[#65696F] hover:text-black'>FAQ</a>
-            </div>
-          </div>
-        </div>
-        <div className='flex items-center justify-between py-6 border-t border-t-[  ] mt-26.5' >
-          <div>
-            <a href='' className='text-[#65696F]'>
-              ©2025 AJ “O‘zbektelekom” · Barcha huquqlar himoyalangan.
+            <p className='text-[16px] font-medium text-[#65696F] dark:text-gray-400'>
+              +998 55 500 09 90
+            </p>
+            <a
+              href="#"
+              className='text-[16px] font-medium text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'
+            >
+              1c@uztelecom.uz
             </a>
           </div>
-          <div className='flex items-center gap-6'>
-            <a href='' className='text-[#65696F] hover:text-black'>Foydalanish shartlari</a>
-            <a href='' className='text-[#65696F] hover:text-black'>Maxfiylik siyosati</a>
-            <a href='' className='text-[#65696F] hover:text-black'>Xavfsizlik</a>
+
+          {/* Links */}
+          <div className='flex gap-30'>
+
+            <div className='flex flex-col gap-3'>
+              <a className='text-[16px] font-bold text-[#26282A] dark:text-white'>
+                Xizmat xaqida
+              </a>
+              <a className='text-[16px] text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+                1C UZTELECOM nima?
+              </a>
+              <a className='text-[16px] text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+                Foydali funksiyalar
+              </a>
+              <a className='text-[16px] text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+                Afzalliklar
+              </a>
+              <a className='text-[16px] text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+                Qanday boshlash
+              </a>
+            </div>
+
+            <div className='flex flex-col gap-3'>
+              <a className='text-[16px] font-bold text-[#26282A] dark:text-white'>
+                Resurslar
+              </a>
+              <a className='text-[16px] text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+                Tariflar
+              </a>
+              <a className='text-[16px] text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+                FAQ
+              </a>
+            </div>
+
           </div>
         </div>
+
+        {/* Bottom */}
+        <div className='flex flex-col lg:flex-row items-center justify-between py-6 border-t border-gray-300 dark:border-gray-700 mt-20'>
+
+          <a className='text-[#65696F] dark:text-gray-400 text-sm'>
+            ©2025 AJ “O‘zbektelekom”
+          </a>
+
+          <div className='flex gap-6 mt-4 lg:mt-0'>
+            <a className='text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+              Foydalanish shartlari
+            </a>
+            <a className='text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+              Maxfiylik
+            </a>
+            <a className='text-[#65696F] dark:text-gray-400 hover:text-black dark:hover:text-white'>
+              Xavfsizlik
+            </a>
+          </div>
+
+        </div>
+
       </div>
     </footer>
   )
